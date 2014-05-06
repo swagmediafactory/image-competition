3504d9aad9023a6dfd590a3a9e78463acd8e0452
jake@swagmediafactory.com <moreland22>
2014-05-06 18:34:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageCompetition::Application.config.secret_token = '90f4df37057f8ddef14fc1a05ef56e217f71a357239321e4d4f3e15c799baa7a5739d6b34249e522886148ab802b1e1686340b3aff977d4c12c502ca5c987488'
